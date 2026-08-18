bcfc57f73bb7f4b1d9a0dd9340eae6c650bb33f2
MARIA EDUARDA FREITAS DA SILVA <43180121823@info.local>
2026-08-18 15:50:59 -0300

    Footer reajustado

--- ChamaJussa/src/Componentes/footer/Footer.jsx
+++ ChamaJussa/src/Componentes/footer/Footer.jsx
@@ -3,21 +3,35 @@ import { FooterStyle } from "./FooterStyle"
 export const Footer = () => {
     return (
         <View style={FooterStyle.cardMenu}>
-            <TouchableOpacity style={FooterStyle.card} >
+              <View style={FooterStyle.card} >
+            <TouchableOpacity >
 
                 <Image source={require('../../../assets/MinhasOS.png')}
                     style={FooterStyle.cardButtonImage} />
+            </TouchableOpacity>
+
+            <TouchableOpacity  >
+
                 <Image source={require('../../../assets/CriarOS.png')}
                     style={FooterStyle.cardButtonImage}
                 />
+            </TouchableOpacity>
+
+            <TouchableOpacity >
+
                 <Image source={require('../../../assets/Notificacoes.png')}
                     style={FooterStyle.cardButtonImage}
                 />
+            </TouchableOpacity>
+
+            <TouchableOpacity  >
+
                 <Image source={require('../../../assets/Perfil.png')}
                     style={FooterStyle.cardButtonImage}
                 />
 
             </TouchableOpacity>
+            </View>
             <View style={FooterStyle.card} >
 
                 <Text style={FooterStyle.texto}>Minhas OS</Text>
