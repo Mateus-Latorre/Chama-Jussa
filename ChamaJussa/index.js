1075e6199ec137d8437fe7d1efda0e776113c4cc
MARIA EDUARDA FREITAS DA SILVA <43180121823@info.local>
2026-08-20 16:37:17 -0300

    projeto quase finalizado

--- ChamaJussa/index.js
+++ ChamaJussa/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './src/app/detalhes';
+import App from './src/app/cadServico';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
